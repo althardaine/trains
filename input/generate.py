0951f34b8dfbd78e6c5054b607b6ed0fda6b8134
Marcin <marcin@mradomski.pl>
2015-12-03 23:00:39 +0100

    Make the generator output # of people per segment, not stop

--- input/generate.py
+++ input/generate.py
@@ -8,7 +8,7 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--num-stops', type=int, default=20)
+parser.add_argument('-s', '--num-segments', type=int, default=20)
 parser.add_argument('-t', '--num-trains', type=int, default=20)
 parser.add_argument('-c', '--train-capacity', type=int, default=50)
 parser.add_argument('-m', '--max-trains-per-line', type=int, default=10)
@@ -17,23 +17,24 @@ ARGS = parser.parse_args(sys.argv[1:])
 trains = []
 
 for i in range(ARGS.num_trains):
-    path_len = random.randint(2, ARGS.num_stops)
-    path = random.sample(range(ARGS.num_stops), path_len)
+    path_len = random.randint(1, ARGS.num_segments)
+    path = random.sample(range(ARGS.num_segments), path_len)
     trains.append(path)
 
-stops = [0] * ARGS.num_stops
+segments = [0] * ARGS.num_segments
 solution = []
 
 for train in trains:
     solution.append(random.randint(1, ARGS.max_trains_per_line))
     for stop in train:
-        stops[stop] += solution[-1] * ARGS.train_capacity
+        segments[stop] += solution[-1] * ARGS.train_capacity
 
-print(','.join(str(x) for x in stops))
+print(','.join(str(x) for x in segments))
 print(ARGS.num_trains)
 print('\n'.join('%d:%s' % (i, ','.join(str(x) for x in path)) for i, path in enumerate(trains)))
 print('')
 print('numberOfBuses=%d' % sum(solution))
 print('busCapacity=%d' % ARGS.train_capacity)
 
-print('\n'.join(str(x) for x in solution), file=sys.stderr)
+print('\n# optimal solution:')
+print('\n'.join('# ' + str(x) for x in solution))
